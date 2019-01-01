b66ecf260e8ae07ceb135e3d2b7bcb81b7dc7f12
Ethan2018 <aboradchen@163.com>
2019-01-01 22:43:19 +0800

    单例

---
+++ TestSingle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TestSingleTwo
+{
+    private static TestSingleTwo instance;
+    public static TestSingleTwo Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new TestSingleTwo();  
+            }
+            return instance;
+        }
+    }
+    public void ShowMe()
+    {
+
+    }
+}
+
+
+public class TestSingle : MonoBehaviour { //只能挂 不能new
+
+    // Use this for initialization
+
+    public static TestSingle Instance; //全局变量 用在组织框架的管理者
+    private void Awake()
+    {
+        Instance = this;
+    }
+    void Start () {
+
+        TestSingleTwo.Instance.ShowMe();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
